ba0f9184c0cba791da3eb30dea23e64c761f11a4
Oyekunle Elijah PlayMice <elijah@playmiceweb.com>
2017-04-26 03:00:24 +0100

    Finished trial division algorithm

--- NumberTheory/PrimalityTest-TrialDivision.cpp
+++ NumberTheory/PrimalityTest-TrialDivision.cpp
@@ -2,3 +2,30 @@
 // Created by playmice on 4/26/17.
 //
 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+bool trialDivision(int number);
+
+int main() {
+    int num;
+    cout << "Enter the number you want to test: " << endl;
+    cin >> num;
+    bool primality = trialDivision(num);
+    cout << primality;
+}
+
+bool trialDivision(int number) {
+    if (number == 1) {
+        return 0;
+    }
+    int squareRoot = (int) sqrt(number);
+    for(int i = 2; i <= squareRoot; i++) {
+        if (number%i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
